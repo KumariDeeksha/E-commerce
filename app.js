95f6c6e68f610be649a890cfc79cdaca71b3ea32
KumariDeeksha <deeku2117@gmail.com>
2023-05-09 19:33:20 +0530

    root done

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ const errorHandler = require('./helpers/errorhandler');
 app.use(cors());
 app.options('*', cors())
 
-//middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
 app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 app.use(errorHandler);
 
-//Routes
+
 const categoriesRoutes = require('./src/routes/categories');
 const productsRoutes = require('./src/routes/products');
 const usersRoutes = require('./src/routes/users');
@@ -33,55 +32,10 @@ app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
 
-
-//Server
 app.listen(7000, ()=>{
 
     console.log('server is running http://localhost:7000');
 })
 
-
-
-// const express=require('express')
-// const app=express();
-// const mongoose=require('mongoose')
-// require('./src/db/mongoose');
-// //const bodyparser=require('body-parser');
-// require('dotenv/config');
-// const cors=require('cors')
-// const morgan=require('morgan')
-// app.use(cors());
-// app.options('*',cors())
-
-// const api=process.env.API_URL;
-// const authJwt=require('./src/middleware/jwt')
-// const errorhandler=require('./src/middleware/errorhandler')
-
-// //middleware
-// app.use(express.json());
-// //app.use(bodyparser.json());
-// app.use(morgan('tiny'));
-// app.use(authJwt());
-// app.use(errorhandler);
-
-
-
-// const productRouter=require('./src/routes/products')
-// const categoryRouter=require('./src/routes/categories')
-// const usersRouter=require('./src/routes/users')
-// const orderRouter=require('./src/routes/orders')
-
-
-
-// app.use(`${api}/products`,productRouter);
-// app.use(`${api}/categories`,categoryRouter)
-// app.use(`${api}/users`,usersRouter)
-// app.use(`${api}/orders`,orderRouter)
-
-
-
-// app.listen(7000,()=>{
-// console.log('listening on port:7000');
-// })
 
 
