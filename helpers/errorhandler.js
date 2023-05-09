89e29f3c34ebb982a5801f1c3e8b62d187554d82
KumariDeeksha <deeku2117@gmail.com>
2023-05-09 19:27:29 +0530

    error handling done

--- helpers/errorhandler.js
+++ helpers/errorhandler.js
@@ -1,16 +1,12 @@
 function errorHandler(err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
-        // jwt authentication error
-        //return res.status(401).json({message: "The user is not authorized"})
-        console.log(err);
+        return res.status(401).json({message: "The user is not authorized"})
     }
 
     if (err.name === 'ValidationError') {
-        //  validation error
         return res.status(401).json({message: err})
     }
 
-    // default to 500 server error
     return res.status(500).json(err);
 }
 
@@ -20,14 +16,3 @@ module.exports = errorHandler;
 
 
 
-// function errorhandler(err,req,res,next){
-// if(err.name==='UnauthorizedError'){
-//        return res.status(401).json({message:err})
-//     }
-//     if(err.name==='ValidationError'){
-//         return res.status(401).json({message:err})
-//     }
-//     return res.status(500).json(err);
-// }
-
-// module.exports=errorhandler;
